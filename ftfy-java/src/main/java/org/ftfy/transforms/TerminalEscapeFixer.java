63137f320790ee8b9d4c44a37581b50d492ed3a5
Leo Gertsenshteyn <146586+leoger@users.noreply.github.com>
2026-03-06 16:47:55 -0800

    Add TerminalEscapeFixer and unit tests to strip ANSI CSI sequences (#5)

---
+++ ftfy-java/src/main/java/org/ftfy/transforms/TerminalEscapeFixer.java
@@ -0,0 +1,27 @@
+package org.ftfy.transforms;
+
+import java.util.regex.Pattern;
+
+/**
+ * Removes ANSI CSI terminal escape sequences from text.
+ */
+public final class TerminalEscapeFixer {
+    private static final Pattern ANSI_CSI_SEQUENCE =
+            Pattern.compile("\\u001B\\[[0-?]*[ -/]*[@-~]");
+
+    private TerminalEscapeFixer() {
+    }
+
+    /**
+     * Strip ANSI CSI sequences from text.
+     *
+     * @param text input text
+     * @return text with ANSI CSI sequences removed
+     */
+    public static String stripAnsiCsiSequences(String text) {
+        if (text == null) {
+            return null;
+        }
+        return ANSI_CSI_SEQUENCE.matcher(text).replaceAll("");
+    }
+}
