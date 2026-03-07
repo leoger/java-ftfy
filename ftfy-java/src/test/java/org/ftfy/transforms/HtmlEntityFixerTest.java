19fed2ca321d1664dc465fc51347b538fcb1da60
Leo Gertsenshteyn <146586+leoger@users.noreply.github.com>
2026-03-06 16:09:26 -0800

    Add HtmlEntityFixer: semicolon-terminated HTML entity decoder for Java (#3)

---
+++ ftfy-java/src/test/java/org/ftfy/transforms/HtmlEntityFixerTest.java
@@ -0,0 +1,31 @@
+package org.ftfy.transforms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HtmlEntityFixerTest {
+    @Test
+    void decodesStandardEntities() {
+        assertEquals("<", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&lt;"));
+        assertEquals("&", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&amp;"));
+        assertEquals("’", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&#x2019;"));
+        assertEquals("’", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&#8217;"));
+    }
+
+    @Test
+    void leavesAmbiguousOrNonTerminatedEntitiesUnchanged() {
+        assertEquals("&notit", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&notit"));
+        assertEquals("AT&T", HtmlEntityFixer.decodeSemicolonTerminatedEntities("AT&T"));
+        assertEquals("&unknown;", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&unknown;"));
+        assertEquals("&#xZZ;", HtmlEntityFixer.decodeSemicolonTerminatedEntities("&#xZZ;"));
+    }
+
+    @Test
+    void decodesMultipleEntitiesInMixedText() {
+        String input = "Tom &amp; Jerry &lt;3 &#x2019;quote&#8217;";
+        String expected = "Tom & Jerry <3 ’quote’";
+
+        assertEquals(expected, HtmlEntityFixer.decodeSemicolonTerminatedEntities(input));
+    }
+}
